37ae7c723024265ac86fc68a6b82c9ab86e851b8
Dakota Bryant <bryant.dakota@gmail.com>
2017-08-03 01:16:32 -0500

    Finally fixed commas, now finishing styling

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,8 @@ let appState = {
     'Installation Pro',
     'Residential Pro',
     'Commercial Pro'
-  ]
+  ],
+  filteredDealers: []
 };
 
 const stateEditingObject = {
@@ -33,15 +34,21 @@ const stateEditingObject = {
         renderObject.renderDealers(this.state);
       });
   },
-  filterDealers: function(event) {
-    console.log(this)
-    if (this.state.filters.includes(event.target.value)) {
-      this.state.filters.filter(word => {
-        return word !== event.target.value;
-      });
+  filterDealers: function(e) {
+    let newFilters = appState.filters.filter(word => {
+      return word !== event.target.value;
+    });
+    if (appState.filters.includes(event.target.value)) {
+      appState.filters = [...newFilters];
+      dealerNumber.innerHTML = `${appState.filteredDealers.length} dealers`;
+      renderObject.renderDealers(appState);
+      return;
+    } else {
+      appState.filters = [...appState.filters, event.target.value];
+      dealerNumber.innerHTML = `${appState.filteredDealers.length} dealers`;
+      renderObject.renderDealers(appState);
+      return;
     }
-    (this.state.filters = [...this.state.filters, event.target.value]);
-    console.log(this.state)
   }
 };
 const renderObject = {
@@ -51,32 +58,68 @@ const renderObject = {
     const cardsParent = document.getElementById('dealer-cards');
     let newDealers = document.createElement('div');
     let certificationContent;
+    let businessHours;
+
+    newDealers.id = 'card-container';
     cardsParent.innerHTML = '';
 
     state.dealers.map(dealer => {
+      const { certifications, weekHours, name, phone1, email } = dealer.data;
+
+      // if (!certifications.includes(...state.filters)) {
+      //   console.log(certifications)
+      //   return;
+      // }
       certificationContent = '';
+      certifications.forEach(certification => {
+        certificationContent += `<p>${certification}</p>`;
+      });
+      businessHours = '';
+      Object.keys(weekHours).forEach(key => {
+        if (businessHours.includes('Weekdays')) {
+          console.log('weekday');
+        }
+        let keyText;
+        let hoursText;
+        weekHours[key] == ''
+          ? (hoursText = 'Closed')
+          : (hoursText = weekHours[key]);
+        switch (key) {
+          case 'mon':
+          case 'tue':
+          case 'wed':
+          case 'thu':
+          case 'fri':
+            keyText = 'Weekdays';
+            break;
+          case 'sat':
+            keyText = 'Saturdays';
+            break;
+          case 'sun':
+            keyText = 'Sundays';
+            break;
+          default:
+            return (keyText = 'Error');
+        }
+
+        businessHours += `<p>${keyText}: ${hoursText}</p>`;
+      });
+
       newDealers.innerHTML += `
       <div class="card">
-      <h6 class="title">${dealer.data.name}</h6>
-      <a href="tel:${dealer.data.phone1}">
-        <button class="call">
-          <i class="phone"></i> Tap to call ${dealer.data.phone1}
-        </button>
+      <h6 class="title">${name}</h6>
+      <a href="tel:${phone1}" class="call">
+          <i class="phone-icon"></i> Tap to call <span>${phone1}</span>
       </a>
-      <i>Can't talk now? Click below to send an email.</i>
-      <a href="mailto:${dealer.data.email}">
-      <button>Contact this Pro</button>
+      <i class="email" >Can't talk now? Click below to send an email.</i>
+      <a href="mailto:${email}" class="contact-pro">
+      Contact this Pro
       </a>
       <div class="hours">
         <h6>Business Hours</h6>
-        <p>Weekdays 7:00am - 7:00pm</p>
-        <p>Weekdays 7:00am - 7:00pm</p>
-        <p>Weekdays 7:00am - 7:00pm</p>
+        ${businessHours}
       </div>
       <div class="card-bottom">        
-        ${dealer.data.certifications.map(certification => {
-          certificationContent += `<p>${certification}</p>`;
-        })}
         ${certificationContent}
       </div>
       </div>`;
