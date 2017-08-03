b9deecf1d47aa3e16492d9c771dfa5616c1dd9c2
Dakota Bryant <bryant.dakota@gmail.com>
2017-08-02 22:12:51 -0500

    latest progress

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,93 @@
 /* eslint-disable */
-let dealers;
+
 const dealerNumber = document.getElementById('dealer-number');
 const button = document.getElementsByClassName('button-container')[0];
-const filters = document.getElementsByClassName('pop-up-filter')[0];
-
-const showData = data => {
-  console.log(data);
-};
-const getData = () => {
-  fetch('./data/dealers.json')
-    .then(data => {
-      return data.json();
-    })
-    .then(data => {
-      dealers = data;
-    })
-    .then(() => {
-      showData(dealers);
-      dealerNumber.innerHTML = `${dealers.dealers.length} dealers`;
-    })
-    .catch(err => {
-      console.error(err);
-    });
-};
-
+const filterContainer = document.getElementsByClassName('pop-up-filter')[0];
+const filters = document.querySelectorAll('input');
 const showFilters = () => {
-  filters.classList.toggle('hidden');
+  filterContainer.classList.toggle('hidden');
 };
+
+let appState = {
+  dealers: [],
+  filters: [
+    'Service Pro',
+    'Installation Pro',
+    'Residential Pro',
+    'Commercial Pro'
+  ]
+};
+
+const stateEditingObject = {
+  state: appState,
+
+  getDealers: function(url) {
+    fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        this.state.dealers = [...this.state.dealers, ...data.dealers];
+        return (dealerNumber.innerHTML = `${this.state.dealers
+          .length} dealers`);
+      })
+      .then(() => {
+        renderObject.renderDealers(this.state);
+      });
+  },
+  filterDealers: function(event) {
+    console.log(this)
+    if (this.state.filters.includes(event.target.value)) {
+      this.state.filters.filter(word => {
+        return word !== event.target.value;
+      });
+    }
+    (this.state.filters = [...this.state.filters, event.target.value]);
+    console.log(this.state)
+  }
+};
+const renderObject = {
+  state: appState,
+
+  renderDealers: function(state) {
+    const cardsParent = document.getElementById('dealer-cards');
+    let newDealers = document.createElement('div');
+    let certificationContent;
+    cardsParent.innerHTML = '';
+
+    state.dealers.map(dealer => {
+      certificationContent = '';
+      newDealers.innerHTML += `
+      <div class="card">
+      <h6 class="title">${dealer.data.name}</h6>
+      <a href="tel:${dealer.data.phone1}">
+        <button class="call">
+          <i class="phone"></i> Tap to call ${dealer.data.phone1}
+        </button>
+      </a>
+      <i>Can't talk now? Click below to send an email.</i>
+      <a href="mailto:${dealer.data.email}">
+      <button>Contact this Pro</button>
+      </a>
+      <div class="hours">
+        <h6>Business Hours</h6>
+        <p>Weekdays 7:00am - 7:00pm</p>
+        <p>Weekdays 7:00am - 7:00pm</p>
+        <p>Weekdays 7:00am - 7:00pm</p>
+      </div>
+      <div class="card-bottom">        
+        ${dealer.data.certifications.map(certification => {
+          certificationContent += `<p>${certification}</p>`;
+        })}
+        ${certificationContent}
+      </div>
+      </div>`;
+    });
+    cardsParent.appendChild(newDealers);
+  }
+};
+window.addEventListener('load', function() {
+  stateEditingObject.getDealers('./data/dealers.json');
+});
 
 button.addEventListener('click', showFilters);
 
-console.log(button);
-getData();
+filters[0].addEventListener('change', stateEditingObject.filterDealers);
